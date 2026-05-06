1dd1eff2ab6cc0d1c260e60a09f55207d24a62b3
Marc <webdev@marcbakker.com>
2026-05-06 14:17:17 +0200

    toeveogen tsconfig.json naast tsconfig.app.json zodat path alias @UI-Engine/components werken

--- apps/UI-Engine/src/app/component-registry.ts
+++ apps/UI-Engine/src/app/component-registry.ts
@@ -1,5 +1,4 @@
-import {WidgetText} from './components/';
-import {WidgetChart} from './components/';
+import { WidgetText, WidgetChart } from '@UI-Engine/components';
 
 export const WIDGET_REGISTRY: Record<string, any> = {
   'text': WidgetText,
